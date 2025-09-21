8edeadfb1f18f9817e66539ae85fa5aaa9f367c4
محمد برجی <mohammad.programer.front@gmail.com>
2025-09-21 19:38:12 +0330

    initial commit - Clone Snapp project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className="text-9xl text-center">سلام این یک تست هست</h1>
+    </>
+  )
+}
+
+export default App;
