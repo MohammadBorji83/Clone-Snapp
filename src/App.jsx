71be562b2a35bef469b6d31a7a1192a446485670
محمد برجی <mohammad.programer.front@gmail.com>
2025-09-21 20:09:48 +0330

    add font

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 
   return (
     <>
-     <h1 className="text-9xl text-center">سلام این یک تست هست</h1>
+     <h1 className="text-4xl text-center font-vazir">سلام این یک تست هست</h1>
     </>
   )
 }
